4f90ac9aa8de9eaf2336af1f7868780184e2f815
Vedran Mijatović <vedranmijatovic0@gmail.com>
2018-04-24 11:55:40 +0200

    add more pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
     @posts = Post.all.sample(10)
   end
 
+  def users
+    @users = Post.all.sample(10).map(&:name)
+  end
+
+  def never_visited
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
+  get "users", to: "pages#users"
+  get "never_visited", to: "pages#never_visited"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
